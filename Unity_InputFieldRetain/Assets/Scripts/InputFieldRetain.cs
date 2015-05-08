f4f8ef6bb1f3efdf8260a5e15514003568e22b57
Keisuke Kumagai <blacksesame114kuma@gmail.com>
2015-05-08 10:49:26 +0900

    Initial Commit

---
+++ Unity_InputFieldRetain/Assets/Scripts/InputFieldRetain.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class InputFieldRetain : MonoBehaviour{
+    public Text text;
+    public Text placeholder;
+    public TouchScreenKeyboard keyboard;
+    public string name;
+    public TouchScreenKeyboardType keyboardtype = TouchScreenKeyboardType.Default;
+    // Use this for initialization
+    void Start () {
+	
+    }	
+    // Update is called once per frame
+    void Update () {
+	if(keyboard != null){
+	    if(keyboard.done){
+		//Save
+		if(name != "")PlayerPrefs.SetString(name,text.text);
+	    }
+	    if(keyboard.text.Length > 0 && keyboard.active){
+		text.text = keyboard.text;
+	    }
+	}
+	if(text.text.Length <= 0){
+	    text.enabled = false;
+	    placeholder.enabled = true;
+	}else{
+	    text.enabled = true;
+	    placeholder.enabled = false;
+	}
+    }
+    public void openKeyboard(){
+	keyboard = TouchScreenKeyboard.Open(text.text,keyboardtype);
+    }
+}
